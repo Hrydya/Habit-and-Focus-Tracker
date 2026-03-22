a6292282065890a4b61b44983e793db6fceb581a
Hrydya <143895876+Hrydya@users.noreply.github.com>
2026-03-22 19:47:44 +0530

    add auth routes

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -1,28 +1,28 @@
-const jwt=require('jsonwebtoken')
-const User=require('../models/user.js')
-const bcrypt= require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user.js')
+const bcrypt = require('bcryptjs')
 
 
-const register= async(req,res)=>{
-    try{
-            const{name,email,password}=req.body;
-            const hashedpassword = await bcrypt.hash(password,10);
-        const user = await User.create({ name, email,password: hashedpassword });
-            res.status(201).json({user});
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedpassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashedpassword });
+        res.status(201).json({ user });
     }
-    catch(error){
-        res.status(500).json({msg:error.message})
+    catch (error) {
+        res.status(500).json({ msg: error.message })
     }
 }
-const login= async(req,res)=>{
-    try{
+const login = async (req, res) => {
+    try {
         const { email, password } = req.body;
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(401).json({msg:"Invalid Credentials"});
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ msg: "Invalid Credentials" });
         }
-        const isMatch = await bcrypt.compare(password,user.password);
-        if(!isMatch){
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
             return res.status(401).json({ msg: "Invalid Credentials" });
         }
 
@@ -32,10 +32,10 @@ const login= async(req,res)=>{
             { expiresIn: process.env.JWT_LIFETIME }
         )
 
-        res.status(200).json({token})
+        res.status(200).json({ token })
     }
-    catch(error){
+    catch (error) {
         res.status(500).json({ msg: error.message })
     }
 }
-module.exports ={register,login}
+module.exports = { register, login }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
-require('dotenv').config() 
-const connectdb= require('./config/db.js')
+require('dotenv').config()
+const connectdb = require('./config/db.js')
+const authroutes = require('./routes/authroutes.js')
+
 const express = require('express')
-const app= express();
-const port =process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use('/api/auth', authroutes)
+
 connectdb();
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
     res.send('API is running')
 })
-app.listen(port,()=>{
+
+app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
