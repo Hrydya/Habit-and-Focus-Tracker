2c1b5c8acaf575a6356a4708db2e276a635c9ec4
Hrydya <143895876+Hrydya@users.noreply.github.com>
2026-04-12 13:38:40 +0530

    fix routes issue

--- server/routes/habitroutes.js
+++ server/routes/habitroutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const protect= require('../middleware/authmiddleware')
 const  {createHabit,deleteHabit, getHabits,completeHabit,getAnalytics} = require('../controllers/habitcontroller.js')
+router.route('/analytics').get(protect, getAnalytics);
 router.route('/').post(protect,createHabit)
 router.route('/').get(protect, getHabits)
 router.route('/:id').delete(protect, deleteHabit);
 router.route('/:id/complete').patch(protect,completeHabit);
-router.route('/analytics').get(protect, getAnalytics);
+
 module.exports = router
